55f58d2f22aec40719bef4a45c73251c110222a1
Sean Blake <sean.blake97@outlook.com>
2022-02-12 19:00:09 -0500

    Initial Commit

---
+++ DesignCombine/DesignCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCombineApp.swift
+//  DesignCombine
+//
+//  Created by SEAN BLAKE on 2/12/22.
+//
+
+import SwiftUI
+
+@main
+struct DesignCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
